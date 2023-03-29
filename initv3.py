22406bf23433507600dc10ecc56ec9353744ea15
Sarah Selby <57141798+sarahselby@users.noreply.github.com>
2023-03-29 17:43:29 +0100

    Update initv3.py

--- initv3.py
+++ initv3.py
@@ -4,84 +4,54 @@ import random
 from paho.mqtt import client as mqtt_client
 from nltk.sentiment import SentimentIntensityAnalyzer
 import json
-import neopixel
 import RPi.GPIO as io
 import board
 import time
 
-num_pixels = 60
 io.setmode(io.BCM)
 
-# Initialize the neopixels object
-pixels_pos = neopixel.NeoPixel(board.D21, num_pixels)
-pixels_neg = neopixel.NeoPixel(board.D12, num_pixels)
+# define the pins for the water and poison pump
 waterPump = 27
 poisonPump = 22
 
+# set pump pins as outputs
 io.setup(waterPump, io.OUT)
 io.setup(poisonPump, io.OUT)
 
-# Set the color of all neopixels to black
-pixels_pos.fill((0, 0, 0))
-pixels_pos.show()
-pixels_neg.fill((0, 0, 0))
-pixels_neg.show()
-
-delay = 0.03
-
+# define the MQTT info
 broker = 'io.adafruit.com'
 port = 1883
 topic = "sarahselby/feeds/rose_piece"
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 100)}'
 username = 'sarahselby'
-password = 'aio_ciZt75e2CiDGrriOGxVLCVCteY5t'
+password = 'xxxxx'
 sia = SentimentIntensityAnalyzer()
 
+
+# function to be called when 
 def activateLed(sentiment):
 
 	print("activateLed function called")
 
-	global pixels_pos
-	global pixels_neg
+	# get the global pump variables to use within function
 	global waterPump
 	global poisonPump
 
+	# check whether the sentiment of the message was above zero (positive)
 	if sentiment > 0:
+		# turn water pump on for 0.3 seconds
 		io.output(waterPump, True)
-		print("Positive message recieved, green LED activated")
+		print("Positive message recieved, water pump activated")
 		time.sleep(0.3)
 		io.output(waterPump, False)
-		# Loop through each neopixel from the far end to the start of the strip
-		for i in range(num_pixels-1, -1, -1):
-			# Set the color of the current neopixel to white
-			pixels_pos[i] = (255, 255, 255)
-			pixels_pos.show()
-			time.sleep(delay)
-			# Set the color of the current neopixel back to black
-			pixels_pos[i] = (0, 0, 0)
-
-			# Turn off all neopixels
-			pixels_pos.fill((0, 0, 0))
-			pixels_pos.show()
-
-
+	# if sentiment was below zero (negative)...
 	elif sentiment < 0:
+		# turn poison pump on for 0.3 seconds
 		io.output(poisonPump, True)
-		print("Negative message recieved, red LED activated")
+		print("Negative message recieved, poison pump activated")
 		time.sleep(0.3)
 		io.output(poisonPump, False)
-		for i in range(num_pixels-1, -1, -1):
-			# Set the color of the current neopixel to white
-			pixels_neg[i] = (255, 255, 255)
-			pixels_neg.show()
-			time.sleep(delay)
-			# Set the color of the current neopixel back to black
-			pixels_neg[i] = (0, 0, 0)
-
-			# Turn off all neopixels
-			pixels_neg.fill((0, 0, 0))
-			pixels_neg.show()
 
 
 # connect to mqtt
@@ -109,15 +79,16 @@ def on_message(client, userdata, message):
 
 # do sentiment analysis
 def analyse_sentiment(msg):
+	# get polarity scores using NLTK library
     sia_score = sia.polarity_scores(msg)
     print(sia.polarity_scores(msg))
+    # check if compound (combined) score indicates a positive or negative message
     print(sia_score["compound"])
     if sia_score["compound"] > 0:
         print("result = positive")
-        # send to relay here
     else:
         print("result = negative")
-        # send to relay here
+    # call function to activate the pumps
     activateLed(sia_score["compound"])
 
 
